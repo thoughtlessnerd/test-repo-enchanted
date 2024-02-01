9495e80d9b9b58d3b510da659edb995b6f60d377
Abhay Chauhan <79973672+thoughtlessnerd@users.noreply.github.com>
2024-02-01 21:57:54 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,6 @@
 // Copyright 2024 Abhay
 #include <iostream>
-
-void printMessage(const std::string& message) {
-    std::cout << message << std::endl;
-}
-
-int addNumbers(int a, int b) {
-    return a + b;
-}
-
 int main() {
-    printMessage("Hello, Clang Format!");
-    
-    int result = addNumbers(5, 7);
-    std::cout << "Result: " << result << std::endl;
-
+    std::cout << "hello world" << std::endl;
     return 0;
 }
